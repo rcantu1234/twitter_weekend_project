4e0025460cb9de978c5a06c26427850f0fc1a202
ROEL CANTU <roel.cantu@yahoo.com>
2015-02-05 19:26:34 -0600

    Editing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ActiveRecord::Base
   has_many :tweets
 
-  validates :first_name, :last_name, :profile_name, :location, :photo_url,
-            :presence => true
+  validates :first_name, :last_name, :location, :photo_url, :profile_name, :presence => true
 
-  validates_uniqueness_of :profile_name
+  # validates_uniqueness_of :profile_name, :presence => true, :length => { :minimum => 2 }
 
-  # validates :bio
 end
